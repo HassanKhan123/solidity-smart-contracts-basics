756fbe0d5c674d6c13dfc972151ea91db3867b42
HassanKhan123 <hk2602809@gmail.com>
2021-04-24 19:55:08 +0500

    fetched accounts from ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,27 +4,11 @@ const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return 'stopped';
-  }
-  drive() {
-    return 'vroom';
-  }
-}
-
-let car;
-
-beforeEach(() => {
-  car = new Car();
+beforeEach(async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts);
 });
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
-  });
-
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  });
+describe('Inbox', () => {
+  it('deploys a contract', () => {});
 });
